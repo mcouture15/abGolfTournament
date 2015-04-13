41a874eecb09507380ef42064c3d796739aad499
Paul Rondeau <paulrondeau07@gmail.com>
2015-04-12 19:13:47 -0700

    added scrollTo section support

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(function() {
+	function setupClicks() {
+		$('header a').on('click', goToSection);
+	}
+
+	function goToSection(e) {
+		e.preventDefault();
+		$('html, body').animate(
+			{
+				scrollTop: $('#' + $(e.target).attr('label-for')).offset().top
+			},
+			'slow'
+		);
+	}
+	setupClicks();
 	console.log('ready');
 });
